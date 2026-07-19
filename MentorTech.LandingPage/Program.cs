fad99551254a61aba6e3d70d1ee6d4dc789cf97d
RICHARD CORREA <desenvolvedor.rc@gmail.com>
2026-07-19 16:20:01 -0300

    feat: configura infraestrutura de dados para usar mysql com ef core

--- MentorTech.LandingPage/Program.cs
+++ MentorTech.LandingPage/Program.cs
@@ -6,7 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+    ));
 
 var app = builder.Build();
 
